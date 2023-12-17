832c4f3a5965dfca1f1137cd7b211b138eac9148
programarreza <programarreza.com>
2023-12-17 23:15:08 +0600

    added to navbar

---
+++ src/components/shared/Navbar/NavbarLink.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+
+const NavbarLink = () => {
+  return (
+    <div className="space-x-5">
+      <NavLink
+        to="/"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "underline" : ""
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/all-classes"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "underline" : ""
+        }
+      >
+        All classes 
+      </NavLink>
+      <NavLink
+        to="/signIn"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "underline" : ""
+        }
+      >
+        Sign In
+      </NavLink>
+    </div>
+  );
+};
+
+export default NavbarLink;
